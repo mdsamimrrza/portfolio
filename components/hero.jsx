afc0e5d2291be14a554fe1226c24829dd02edcb2
mdsamimrrza <mdsamimrrza@gmail.com>
2025-08-18 01:29:10 +0530

    resume fixed

--- components/hero.jsx
+++ components/hero.jsx
@@ -122,11 +122,11 @@ export default function Hero() {
               Contact Me
             </Button>
             <a
-              href="/resume.pdf"
-              download="Samim_Reza_Resume"
+              href="/Md_Samim_Reza.pdf"
+              download="Md_Samim_Reza_Resume"
               className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-11 px-8 py-2"
             >
-              Resume
+              Download Resume
             </a>
           </div>
         </div>
